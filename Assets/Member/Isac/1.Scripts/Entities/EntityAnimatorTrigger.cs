e01e29710410980a5124933524f91c1a13f41eff
Candy <dltkr0424@gmail.com>
2025-07-17 19:42:20 +0900

    feat : EntityAnimator, EntityAnimatorTrigger

---
+++ Assets/Member/Isac/1.Scripts/Entities/EntityAnimatorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Member.Isac._1.Scripts.Entities
+{
+    public class EntityAnimatorTrigger : MonoBehaviour, IEntityComponent
+    { 
+        public Action OnAnimationEndTrigger;
+        
+        private Entity _entity;
+
+
+        public void Initialize(Entity entity)
+        {
+            _entity = entity;
+        }
+        
+        private void AnimationEnd()
+        {
+            OnAnimationEndTrigger?.Invoke();
+        }
+    }
+}
